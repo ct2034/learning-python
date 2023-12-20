506a16992280a233fa607a7bcab7f0f2709373f3
Christian Henkel <post@henkelchristian.de>
2023-12-20 19:43:30 +0100

    worms
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- opencv/display_and_update_img.py
+++ opencv/display_and_update_img.py
@@ -4,64 +4,99 @@
 
 import numpy as np
 import cv2
+import time
 
-def sin2d(x,y):
-    """2-d sine function to plot"""
-    return np.sin(x) + np.cos(y)
 
-def getFrame(p):
+def initFrame(h, w):
+    """Initialize the simulation frame"""
+    return np.zeros((h, w, 3), np.uint8)
+
+
+def dist(a, b):
+    """Distance between two points"""
+    return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+
+
+def getNextFrame(old_frame, particles, h, w):
     """Generate next frame of simulation as numpy array"""
-
-    # Create data on first call only
-    if getFrame.z is None:
-        getFrame.z = np.zeros((h, w,3), np.uint8)
-        getFrame.z[p[1],p[0],:] = 255
+    mid = (w//2, h//2)
     
     # decay existing data
-    getFrame.z = getFrame.z * 0.99
-
+    old_frame = old_frame * 0.9
+    
     # random path
-    prev_p = p
-    dir = np.random.randint(0, 4)
-    l = np.random.randint(0, 50)
-    if dir == 0:
-        p = (prev_p[0]+l, prev_p[1])
-    elif dir == 1:
-        p = (prev_p[0]-l, prev_p[1])
-    elif dir == 2:
-        p = (prev_p[0], prev_p[1]+l)
-    else:
-        p = (prev_p[0], prev_p[1]-l)
+    for i_p, p in enumerate(particles):
+        prev_p = p
+        offcenter = dist(p, mid)
+        l = max(5, int(offcenter/40))
+        l = float(np.random.randint(0, l))
+        dir = np.random.random() * 2. * np.pi
+        p = (
+            int(np.round(p[0] + l * np.cos(dir))),
+            int(np.round(p[1] + l * np.sin(dir)))
+        )
 
-    # check boundaries
-    if p[0] >= h:
-        p = (h-1, p[1])
-    elif p[0] < 0:
-        p = (0, p[1])
-    if p[1] >= w:
-        p = (p[0], w-1)
-    elif p[1] < 0:
-        p = (p[0], 0)
+        # check boundaries
+        if p[0] >= w:
+            p = (w-1, p[1])
+        elif p[0] < 0:
+            p = (0, p[1])
+        if p[1] >= h:
+            p = (p[0], h-1)
+        elif p[1] < 0:
+            p = (p[0], 0)
 
-    # draw line from previous point to current point
-    cv2.line(getFrame.z, prev_p, p, (255,255,255), 1)
+        col = (
+            int(255. * (1. - (offcenter / (w/2)))),
+            int(255. * (offcenter / (w/2))),
+            128
+        )
 
-    return getFrame.z, p
+        # draw line from previous point to current point
+        cv2.line(old_frame, prev_p, p, col, 1)
 
-# Frame size
-w, h = 1920, 1080
-p = (w//2, h//2)
+        # update particle
+        particles[i_p] = p
 
-getFrame.z = None
+    return old_frame, particles
 
-# remove the following two lines for not full screen
-cv2.namedWindow('image', cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty('image',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+def initParticles(h, w, n):
+    """Initialize the particles"""
+    p = []
+    for i in range(n):
+        p.append((np.random.randint(0, w), np.random.randint(0, h)))
+    return p
 
-while True:
-       
-    # Get a numpy array to display from the simulation
-    npimage, p = getFrame(p)
+def main():
+    # Frame size
+    w, h = 1920, 1080
+    p = (w//2, h//2)
 
-    cv2.imshow('image', npimage)
-    cv2.waitKey(1)
+    # remove the following two lines for not full screen
+    cv2.namedWindow('image', cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('image',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+
+    # to screen
+    fps_display_interval = 5  # seconds
+    last_fps_display_time = 0
+
+    npimage = initFrame(h, w)
+    particles = initParticles(h, w, 1000)
+
+    while True:
+        start = time.time()   
+        # Get a numpy array to display from the simulation
+        npimage, particles = getNextFrame(npimage, particles, h, w)
+        # getFrame.z = npimage
+
+        cv2.imshow('image', npimage)
+        cv2.waitKey(1)
+
+        # print fps
+        if last_fps_display_time < time.time() - fps_display_interval:
+            print('FPS {:.1f}'.format(1 / (time.time() - start)))
+            last_fps_display_time = time.time()
+
+if __name__ == '__main__':
+    main()
