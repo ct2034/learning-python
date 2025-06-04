ad4051ef3192bffb2754640cd8db170ad5ac627a
Christian Henkel <post@henkelchristian.de>
2025-06-04 14:01:27 +0200

    patterns in my brain
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- sympy/electric_net.py
+++ sympy/electric_net.py
@@ -6,7 +6,7 @@ vars = symbols('u u1 u2 u3 u4 u5 u6 alpha')
 u, u1, u2, u3, u4, u5, u6, alpha = vars
 eqs = [
     # voltage
-    Eq(u1 + u2 + u3, u),
+    Eq(u1 + u2 + u5, u),
     Eq(u2 + u4, u3),
     Eq(u4 + u6, u5),
     Eq(u1 + u3 + u6, u),
