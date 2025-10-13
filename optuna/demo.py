fc66fbb2663b47cd8660704673f2a5afafb935a8
Christian Henkel <post@henkelchristian.de>
2025-10-13 18:30:17 +0200

    optunaaaa
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- optuna/demo.py
+++ optuna/demo.py
@@ -1,141 +1,81 @@
-"""
-Optuna example that optimizes multi-layer perceptrons using PyTorch.
-
-In this example, we optimize the validation accuracy of fashion product recognition using
-PyTorch and FashionMNIST. We optimize the neural network architecture as well as the optimizer
-configuration. As it is too time consuming to use the whole FashionMNIST dataset,
-we here use a small subset of it.
-
-"""
-
-import os
-
 import optuna
-from optuna.trial import TrialState
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.utils.data
-from torchvision import datasets
-from torchvision import transforms
+
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 
-DEVICE = torch.device("cpu")
-BATCHSIZE = 128
-CLASSES = 10
-DIR = os.getcwd()
-EPOCHS = 10
-N_TRAIN_EXAMPLES = BATCHSIZE * 30
-N_VALID_EXAMPLES = BATCHSIZE * 10
-
-
-def define_model(trial):
-    # We optimize the number of layers, hidden units and dropout ratio in each layer.
-    n_layers = trial.suggest_int("n_layers", 1, 3)
-    layers = []
-
-    in_features = 28 * 28
-    for i in range(n_layers):
-        out_features = trial.suggest_int("n_units_l{}".format(i), 4, 128)
-        layers.append(nn.Linear(in_features, out_features))
-        layers.append(nn.ReLU())
-        p = trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5)
-        layers.append(nn.Dropout(p))
-
-        in_features = out_features
-    layers.append(nn.Linear(in_features, CLASSES))
-    layers.append(nn.LogSoftmax(dim=1))
-
-    return nn.Sequential(*layers)
-
-
-def get_mnist():
-    # Load FashionMNIST dataset.
-    train_loader = torch.utils.data.DataLoader(
-        datasets.FashionMNIST(DIR, train=True, download=True, transform=transforms.ToTensor()),
-        batch_size=BATCHSIZE,
-        shuffle=True,
-    )
-    valid_loader = torch.utils.data.DataLoader(
-        datasets.FashionMNIST(DIR, train=False, transform=transforms.ToTensor()),
-        batch_size=BATCHSIZE,
-        shuffle=True,
+def my_function(x: float, y: float) -> float:
+    assert 0.0 <= x <= 1.0
+    assert 0.0 <= y <= 1.0
+    return (
+        0.9
+        * np.exp(-((x - 0.2) ** 2 + (y - 0.3) ** 2) / 0.04)  # wide peak near (0.2,0.3)
+        + 0.9
+        * np.exp(-((x - 0.7) ** 2 + (y - 0.8) ** 2) / 0.04)  # wide peak near (0.7,0.8)
+        + 0.9
+        * np.exp(
+            -((x - 0.8) ** 2 + (y - 0.2) ** 2) / 0.06
+        )  # narrow peak near (0.8,0.2)
+        + 1.0
+        * np.exp(
+            -((x - 0.35) ** 2 + (y - 0.75) ** 2) / 0.01
+        )  # narrow peak near (0.35,0.75)
     )
 
-    return train_loader, valid_loader
 
-
-def objective(trial):
-    # Generate the model.
-    model = define_model(trial).to(DEVICE)
-
-    # Generate the optimizers.
-    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
-    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-    optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
-
-    # Get the FashionMNIST dataset.
-    train_loader, valid_loader = get_mnist()
-
-    # Training of the model.
-    for epoch in range(EPOCHS):
-        model.train()
-        for batch_idx, (data, target) in enumerate(train_loader):
-            # Limiting training data for faster epochs.
-            if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
-                break
-
-            data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
-
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target)
-            loss.backward()
-            optimizer.step()
-
-        # Validation of the model.
-        model.eval()
-        correct = 0
-        with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(valid_loader):
-                # Limiting validation data.
-                if batch_idx * BATCHSIZE >= N_VALID_EXAMPLES:
-                    break
-                data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
-                output = model(data)
-                # Get the index of the max log-probability.
-                pred = output.argmax(dim=1, keepdim=True)
-                correct += pred.eq(target.view_as(pred)).sum().item()
-
-        accuracy = correct / min(len(valid_loader.dataset), N_VALID_EXAMPLES)
-
-        trial.report(accuracy, epoch)
-
-        # Handle pruning based on the intermediate value.
-        if trial.should_prune():
-            raise optuna.exceptions.TrialPruned()
-
-    return accuracy
+def objective(trial: optuna.Trial) -> float:
+    x = trial.suggest_float("x", 0.0, 1.0)
+    y = trial.suggest_float("y", 0.0, 1.0)
+    return my_function(x, y)
 
 
 if __name__ == "__main__":
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100, timeout=600)
+    # quick 2d plot of the objective function
+    x = np.linspace(0, 1, 100)
+    y = np.linspace(0, 1, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    for x, y in zip(X.flatten(), Y.flatten()):
+        Z[np.where((X == x) & (Y == y))] = my_function(x, y)
+    plt.style.use("bmh")
+    plt.contourf(X, Y, Z, levels=10, cmap="viridis", alpha=0.6)
+    plt.colorbar()
 
-    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+    study = optuna.create_study(
+        direction="maximize",
+        sampler=optuna.samplers.QMCSampler(),
+    )
+    study.optimize(objective, n_trials=128, timeout=10, n_jobs=1)
 
-    print("Study statistics: ")
-    print("  Number of finished trials: ", len(study.trials))
-    print("  Number of pruned trials: ", len(pruned_trials))
-    print("  Number of complete trials: ", len(complete_trials))
+    optimization_path = []
+    for t in study.trials:
+        x = t.params["x"]
+        y = t.params["y"]
+        z = t.value
+        optimization_path.append((x, y, z))
+    optimization_path = np.array(optimization_path)
+    plt.scatter(
+        optimization_path[:, 0],
+        optimization_path[:, 1],
+        c="r",
+        s=5,
+        label="Sampled Points",
+    )
+    plt.plot(
+        study.best_params["x"],
+        study.best_params["y"],
+        "g*",
+        markersize=15,
+        label="Best Parameter",
+    )
+    plt.legend()
 
     print("Best trial:")
-    trial = study.best_trial
-
-    print("  Value: ", trial.value)
-
+    t = study.best_trial
+    print(f"  Value: {t.value}")
     print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+    for key, value in t.params.items():
+        print(f"    {key}: {value}")
+
+    plt.show()
