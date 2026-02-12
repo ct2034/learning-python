b446dd0ff9e9ff94a92756d0952b5b9c34e56f07
Christian Henkel <post@henkelchristian.de>
2026-02-12 18:01:19 +0100

    after ai madness
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- nicegui/demo.py
+++ nicegui/demo.py
@@ -1,166 +1,155 @@
 from nicegui import ui
 from random import randint, random
-from typing import Dict, Any
 
 
-# Color constants
-class Colors:
-    TRANSP = "#505050"
-    DISP = "#EE9010"
-    SCREW = "#20E040"
-    HIGHLIGHT = "#0010EE"
-    IDLE = "#AAAAAA"
+COLORS = {
+    "TRANSP": {"default": "#A0A0A0", "highlight": "#505050"},
+    "DISP": {"default": "#F5C080", "highlight": "#EE9010"},
+    "SCREW": {"default": "#90F0A0", "highlight": "#20E040"},
+    "IDLE": {"default": "#D5D5D5", "highlight": "#AAAAAA"},
+}
 
-
-# Data mappings
-SERIES_CONFIG = {
-    "Screwing": {"color": Colors.SCREW, "gantt_index": 1, "graph_index": 0},
-    "Transport": {"color": Colors.TRANSP, "gantt_index": 2, "graph_index": None},
-    "Dispensing": {"color": Colors.DISP, "gantt_index": 3, "graph_index": 1},
+SERIES = {
+    "Screwing": {
+        "color": COLORS["SCREW"],
+        "gantt_idx": 1,
+        "graph_idx": 0,
+        "edge_idx": None,
+    },
+    "Transport": {
+        "color": COLORS["TRANSP"],
+        "gantt_idx": 2,
+        "graph_idx": None,
+        "edge_idx": 0,
+    },
+    "Dispensing": {
+        "color": COLORS["DISP"],
+        "gantt_idx": 3,
+        "graph_idx": 1,
+        "edge_idx": None,
+    },
 }
 
 
 class ChartLinker:
-    """Handles linking between gantt and graph charts"""
+    def __init__(self, gantt, graph):
+        self.gantt = gantt
+        self.graph = graph
 
-    def __init__(self, gantt_chart, graph_chart):
-        self.gantt = gantt_chart
-        self.graph = graph_chart
-
-    def setup_event_listeners(self):
-        """Set up event listeners after UI is initialized"""
+    def setup_events(self):
         ui.run_javascript(
             f"""
         setTimeout(() => {{
-            const ganttChart = getElement({self.gantt.id}).chart;
-            const graphChart = getElement({self.graph.id}).chart;
+            const gantt = getElement({self.gantt.id}).chart;
+            const graph = getElement({self.graph.id}).chart;
             
-            if (ganttChart && graphChart) {{
-                console.log('Setting up chart event linking');
-                
-                ganttChart.on('mouseover', (params) => {{
-                    emitEvent('gantt_hover', extractSafeParams(params));
-                }});
-                
-                ganttChart.on('mouseout', (params) => {{
-                    emitEvent('gantt_leave', extractSafeParams(params));
-                }});
-                
-                graphChart.on('mouseover', (params) => {{
-                    emitEvent('graph_hover', extractSafeParams(params));
-                }});
-                
-                graphChart.on('mouseout', (params) => {{
-                    emitEvent('graph_leave', extractSafeParams(params));
-                }});
-            }}
-            
-            function extractSafeParams(params) {{
-                return {{
-                    seriesName: params.seriesName,
-                    seriesIndex: params.seriesIndex,
-                    dataIndex: params.dataIndex,
-                    name: params.name,
-                    value: params.value,
-                    componentType: params.componentType
-                }};
+            if (gantt && graph) {{
+                gantt.on('mouseover', p => emitEvent('gantt_hover', {{name: p.seriesName}}));
+                gantt.on('mouseout', () => emitEvent('gantt_leave', {{}}));
+                graph.on('mouseover', p => emitEvent('graph_hover', {{name: p.name}}));
+                graph.on('mouseout', () => emitEvent('graph_leave', {{}}));
             }}
         }}, 1000);
         """
         )
 
-    def highlight_corresponding_element(self, element_name: str, target_chart: str):
-        """Highlight corresponding element in the target chart"""
-        if element_name not in SERIES_CONFIG:
+    def highlight(self, name):
+        if name == "Screwing > Dispensing":
+            name = "Transport"
+
+        if name not in SERIES:
             return
 
-        config = SERIES_CONFIG[element_name]
+        series = SERIES[name]
+        highlight_color = series["color"]["highlight"]
 
-        if target_chart == "graph" and config["graph_index"] is not None:
-            self.graph.options["series"]["nodes"][config["graph_index"]]["itemStyle"][
+        # graph
+        edge_idx = series["edge_idx"]
+        graph_idx = series["graph_idx"]
+        if edge_idx is not None:
+            self.graph.options["series"]["links"][edge_idx]["lineStyle"][
                 "color"
-            ] = Colors.HIGHLIGHT
-            self.graph.update()
-        elif target_chart == "gantt":
-            self.gantt.options["series"][config["gantt_index"]]["itemStyle"][
+            ] = highlight_color
+            self.graph.options["series"]["links"][edge_idx]["lineStyle"][
+                "width"
+            ] = 4
+        if graph_idx is not None:
+            self.graph.options["series"]["nodes"][graph_idx]["itemStyle"][
                 "color"
-            ] = Colors.HIGHLIGHT
-            self.gantt.update()
+            ] = highlight_color
+        self.graph.update()
+        # gantt
+        self.gantt.options["series"][series["gantt_idx"]]["itemStyle"][
+            "color"
+        ] = highlight_color
+        self.gantt.update()
 
     def reset_colors(self):
-        """Reset all chart elements to their original colors"""
-        # Reset graph nodes
-        for name, config in SERIES_CONFIG.items():
-            if config["graph_index"] is not None:
-                self.graph.options["series"]["nodes"][config["graph_index"]][
-                    "itemStyle"
-                ]["color"] = config["color"]
-
-        # Reset gantt series
-        for name, config in SERIES_CONFIG.items():
-            self.gantt.options["series"][config["gantt_index"]]["itemStyle"][
+        for name, series in SERIES.items():
+            default_color = series["color"]["default"]
+            edge_idx = series["edge_idx"]
+            graph_idx = series["graph_idx"]
+            if graph_idx is not None:
+                self.graph.options["series"]["nodes"][graph_idx]["itemStyle"][
+                    "color"
+                ] = default_color
+            self.gantt.options["series"][series["gantt_idx"]]["itemStyle"][
                 "color"
-            ] = config["color"]
+            ] = default_color
+
+        # Reset edge
+        self.graph.options["series"]["links"][0]["lineStyle"]["color"] = "#999"
+        self.graph.options["series"]["links"][0]["lineStyle"]["width"] = 2
 
         self.gantt.update()
         self.graph.update()
 
 
-def create_gantt_chart() -> ui.echart:
-    """Create and configure the Gantt chart"""
+def create_gantt():
     return ui.echart(
         {
             "tooltip": {"trigger": "item"},
-            "legend": {},
             "xAxis": {"type": "value"},
-            "yAxis": {
-                "type": "category",
-                "data": ["Product A", "Product B"],
-            },
+            "yAxis": {"type": "category", "data": ["Product A", "Product B"]},
             "series": [
                 {
                     "name": "Idle",
                     "type": "bar",
                     "stack": "total",
-                    "label": {"show": True},
-                    "emphasis": {"focus": "series"},
                     "data": [200, 0],
-                    "itemStyle": {"color": Colors.IDLE},
+                    "itemStyle": {"color": COLORS["IDLE"]["default"]},
+                    "emphasis": {"disabled": True},
                 },
                 {
                     "name": "Screwing",
                     "type": "bar",
                     "stack": "total",
-                    "label": {"show": True},
-                    "emphasis": {"focus": "series"},
                     "data": [320, 200],
-                    "itemStyle": {"color": Colors.SCREW},
+                    "itemStyle": {"color": COLORS["SCREW"]["default"]},
+                    "emphasis": {"disabled": True},
                 },
                 {
                     "name": "Transport",
                     "type": "bar",
                     "stack": "total",
-                    "label": {"show": True},
-                    "emphasis": {"focus": "series"},
                     "data": [110, 320],
-                    "itemStyle": {"color": Colors.TRANSP},
+                    "itemStyle": {"color": COLORS["TRANSP"]["default"]},
+                    "emphasis": {"disabled": True},
                 },
                 {
                     "name": "Dispensing",
                     "type": "bar",
                     "stack": "total",
-                    "label": {"show": True},
-                    "emphasis": {"focus": "series"},
                     "data": [150, 110],
-                    "itemStyle": {"color": Colors.DISP},
+                    "itemStyle": {"color": COLORS["DISP"]["default"]},
+                    "emphasis": {"disabled": True},
                 },
             ],
         }
     )
 
 
-def create_graph_chart() -> ui.echart:
-    """Create and configure the Graph chart"""
+def create_graph():
     return ui.echart(
         {
             "tooltip": {"trigger": "item"},
@@ -174,20 +163,20 @@ def create_graph_chart() -> ui.echart:
                         "name": "Screwing",
                         "x": -1,
                         "y": 0,
-                        "itemStyle": {"color": Colors.SCREW},
+                        "itemStyle": {"color": COLORS["SCREW"]["default"]},
                     },
                     {
                         "name": "Dispensing",
                         "x": 1,
                         "y": 0,
-                        "itemStyle": {"color": Colors.DISP},
+                        "itemStyle": {"color": COLORS["DISP"]["default"]},
                     },
                 ],
                 "links": [
                     {
                         "source": "Screwing",
                         "target": "Dispensing",
-                        "lineStyle": {"width": 2},
+                        "lineStyle": {"width": 2, "color": "#999"},
                     }
                 ],
             },
@@ -195,68 +184,41 @@ def create_graph_chart() -> ui.echart:
     )
 
 
-def update_gantt_data(gantt: ui.echart):
-    """Update Gantt chart with random data"""
-    t_idle = randint(100, 300)
-    t_screw = randint(200, 400)
-    t_transp = randint(100, 300)
-    t_disp = randint(150, 350)
-    gantt.options["series"][0]["data"] = [t_idle, 0]
-    gantt.options["series"][1]["data"] = [t_screw, t_idle]
-    gantt.options["series"][2]["data"] = [t_transp, t_screw]
-    gantt.options["series"][3]["data"] = [t_disp, t_transp]
+def update_gantt(gantt):
+    for i in range(len(gantt.options["series"]) - 1):
+        val = randint(50, 400)
+        gantt.options["series"][i]["data"][0] = val
+        gantt.options["series"][i + 1]["data"][1] = val
     gantt.update()
 
 
-def update_graph_data(graph: ui.echart):
-    """Update Graph chart with random positions"""
-    graph.options["series"]["nodes"][0]["x"] = random() - 1
-    graph.options["series"]["nodes"][0]["y"] = random()
-    graph.options["series"]["nodes"][1]["x"] = random() + 1
-    graph.options["series"]["nodes"][1]["y"] = random()
+def update_graph(graph):
+    for i, node in enumerate(graph.options["series"]["nodes"]):
+        node["x"] = random() + (-1 if i == 0 else 1)
+        node["y"] = random()
     graph.update()
 
 
 def main():
-    """Main application logic"""
     with ui.grid(columns=2).classes("w-full"):
-        gantt = create_gantt_chart()
-        graph = create_graph_chart()
+        gantt = create_gantt()
+        graph = create_graph()
+        ui.button("Update Gantt", on_click=lambda: update_gantt(gantt))
+        ui.button("Update Graph", on_click=lambda: update_graph(graph))
 
-        ui.button("Update Gantt", on_click=lambda: update_gantt_data(gantt))
-        ui.button("Update Graph", on_click=lambda: update_graph_data(graph))
-
-    # Set up chart linking
     linker = ChartLinker(gantt, graph)
 
-    # Event handlers using the linker
-    def handle_gantt_hover(e):
-        series_name = e.args.get("seriesName")
-        if series_name:
-            linker.highlight_corresponding_element(series_name, "graph")
+    ui.on(
+        "gantt_hover",
+        lambda e: linker.highlight(e.args.get("name")),
+    )
+    ui.on("gantt_leave", lambda e: linker.reset_colors())
+    ui.on("graph_hover", lambda e: linker.highlight(e.args.get("name")))
+    ui.on("graph_leave", lambda e: linker.reset_colors())
 
-    def handle_gantt_leave(e):
-        linker.reset_colors()
-
-    def handle_graph_hover(e):
-        node_name = e.args.get("name")
-        if node_name:
-            linker.highlight_corresponding_element(node_name, "gantt")
-
-    def handle_graph_leave(e):
-        linker.reset_colors()
-
-    # Register event handlers
-    ui.on("gantt_hover", handle_gantt_hover)
-    ui.on("gantt_leave", handle_gantt_leave)
-    ui.on("graph_hover", handle_graph_hover)
-    ui.on("graph_leave", handle_graph_leave)
-
-    # Set up event listeners after UI loads
-    ui.timer(2.0, linker.setup_event_listeners, once=True)
+    ui.timer(2.0, linker.setup_events, once=True)
 
 
-# Use the multiprocessing-compatible guard
 if __name__ in {"__main__", "__mp_main__"}:
     main()
     ui.run()
